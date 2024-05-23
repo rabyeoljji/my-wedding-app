a989e83504a5944a5f69a83fb015ce18e48f5946
nageuna922@gmail.com <nageuna922@gmail.com>
2024-05-23 20:48:37 +0900

    카카오 로그인 관련 코드 수정

--- src/auth/socialLogin/KakaoLoginBtn.tsx
+++ src/auth/socialLogin/KakaoLoginBtn.tsx
@@ -2,7 +2,7 @@ import KAKAO_IMG_WIDE from "../../assets/sns/kakao_login_medium_wide.png";
 
 const KakaoLoginBtn = (): JSX.Element => {
   const KAKAO_API_KEY = import.meta.env.VITE_APP_KAKAO_API_KEY;
-  const KAKAO_REDIRECT_URI = `${import.meta.env.NODE_ENV === "development" ? import.meta.env.VITE_APP_HOST_URL : "https://my-wedding-app.vercel.app"}/kakaoAuth`;
+  const KAKAO_REDIRECT_URI = `${import.meta.env.DEV ? import.meta.env.VITE_APP_HOST_URL : "https://my-wedding-app.vercel.app"}/kakaoAuth`;
   const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
   const handleKakaoLogin = () => {
