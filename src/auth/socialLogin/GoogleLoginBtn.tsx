810959e90d1fa08f4e48fb885bbf0050e3c9fad5
nageuna922@gmail.com <nageuna922@gmail.com>
2024-05-23 20:43:14 +0900

    배포 시를 위한 소셜 로그인 코드 수정

--- src/auth/socialLogin/GoogleLoginBtn.tsx
+++ src/auth/socialLogin/GoogleLoginBtn.tsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router";
 
