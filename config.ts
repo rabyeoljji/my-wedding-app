a195c1b049ef947707d9d1816cecf59e1610320b
nageuna922@gmail.com <nageuna922@gmail.com>
2025-03-22 16:02:43 +0900

    fix: login error

--- config.ts
+++ config.ts
@@ -1,28 +1,16 @@
 "use strict";
 // import dotenv from "dotenv";
 
-const {
-  VITE_APP_PORT,
-  VITE_APP_HOST,
-  VITE_APP_HOST_URL,
-  VITE_APP_API_KEY,
-  VITE_APP_AUTH_DOMAIN,
-  VITE_APP_PROJECT_ID,
-  VITE_APP_STORAGE_BUCKET,
-  VITE_APP_MESSAGING_SENDER_ID,
-  VITE_APP_APP_ID,
-} = import.meta.env;
-
 export const configInfo = {
-  port: VITE_APP_PORT,
-  host: VITE_APP_HOST,
-  url: VITE_APP_HOST_URL,
+  port: import.meta.env.VITE_APP_PORT,
+  host: import.meta.env.VITE_APP_HOST,
+  url: import.meta.env.DEV ? import.meta.env.VITE_APP_HOST_URL : import.meta.env.VITE_APP_AUTH_DOMAIN,
   firebaseConfig: {
-    apiKey: VITE_APP_API_KEY,
-    authDomain: VITE_APP_AUTH_DOMAIN,
-    projectId: VITE_APP_PROJECT_ID,
-    storageBucket: VITE_APP_STORAGE_BUCKET,
-    messagingSenderId: VITE_APP_MESSAGING_SENDER_ID,
-    appId: VITE_APP_APP_ID,
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID,
   },
 };
